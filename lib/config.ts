2726425788fa622fd5ce332c19b28135ae9043f2
Secondspringdesign <christian@secondspring.design>
2025-11-03 20:55:02 -0800

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,5 @@
 import { ColorScheme, StartScreenPrompt, ThemeOption } from "@openai/chatkit";
 
-export const WORKFLOW_ID = process.env.NEXT_PUBLIC_CHATKIT_WORKFLOW_ID?.trim() || '';
-
 export const CREATE_SESSION_ENDPOINT = "/api/create-session";
 
 export const STARTER_PROMPTS: StartScreenPrompt[] = [
