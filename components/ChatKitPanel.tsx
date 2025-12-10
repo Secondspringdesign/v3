8a33a21063a6cde47ec1cb4b291122dc5ddb6956
Secondspringdesign <christian@secondspring.design>
2025-12-10 14:57:35 -0800

    Remove console log for session headers in ChatKitPanel
    Removed development logging from create-session headers.

--- components/ChatKitPanel.tsx
+++ components/ChatKitPanel.tsx
@@ -198,10 +198,6 @@ export function ChatKitPanel({
           headers["Authorization"] = `Bearer ${outsetaToken}`;
         }
 
-        if (isDev) {
-          console.log("[ChatKitPanel] create-session headers:", headers);
-        }
-
         const response = await fetch(`${CREATE_SESSION_ENDPOINT}?agent=${agent}`, {
           method: "POST",
           headers,
